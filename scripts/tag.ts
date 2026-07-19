44ceef5a9699f78cb216beeb84563e4cf106d888
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2026-07-19 18:51:35 +0800

    chore: update tag script

--- scripts/tag.ts
+++ scripts/tag.ts
@@ -3,7 +3,7 @@ import { DateTime } from 'luxon'
 
 function getTag() {
   const now = DateTime.now().setZone('utc')
-  const major = '6'
+  const major = '7'
   const minor = now.toFormat('yyMMdd')
   const patch = now.toFormat('HHmm')
   let version = `${major}.${minor}.${patch}`
